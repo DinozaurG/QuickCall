9741a9114f8ef5b4cf1bb8bf48f3e95dc4010082
DinozaurG <49711433+DinozaurG@users.noreply.github.com>
2021-04-11 19:16:06 +0700

    Created nesessary folders and files for project
    In this commit we created some nesessary folders(like Models, Views, and QuickCallWidget) and files for our project. QuickCallWidget we will use for widget, Models and Views in main folder we will use for Settings after merging branches

---
+++ QuickCall/QuickCallWidget/QuickCallWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  QuickCallWidgetBundle.swift
+//  QuickCall
+//
+//  Created by Алексей Шумейко on 11.04.2021.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct EmojiWidgetBundle: WidgetBundle {
+
+  @WidgetBundleBuilder
+  var body: some Widget {
+    QuickCallMainWidget()
+  }
+}
